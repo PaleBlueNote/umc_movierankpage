b0afdf5257dc2469a240206aecc957b0201790dd
seokchan Yoon <yoonseokchan0731@gmail.com>
2023-11-02 19:10:38 +0900

    Fix 이미지URL수정 - movieDummy.js파일에서 imageUrl을 불러올 때 baseUrl을 추가하여   정상적으로 이미지 로딩되게 하였음

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,36 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
+import '../index.css'; // 새로운 CSS 파일을 import
+
+const baseUrl = "https://image.tmdb.org/t/p/w220_and_h330_face/"
 
 function Movie({ movie }) {
-  const [showOverview, setShowOverview] = useState(false);
-
   return (
-    <div className="movies-container" style={{ position: 'relative' }}>
+    <div className="movies-container">
       <div className="image-container">
         <img
           className="image"
-          src={movie.poster_path}
+          src={baseUrl + movie.poster_path}
           alt={movie.title}
-          onMouseEnter={() => setShowOverview(true)}
-          onMouseLeave={() => setShowOverview(false)}
         />
-      </div>
-      <div
-        className="overview-container"
-        style={{
-          display: showOverview ? 'block' : 'none',
-          position: 'absolute', // 절대 위치 지정
-          top: 0, // 상단에 위치
-          left: 0, // 왼쪽에 위치
-          width: '8.75rem', // 동일한 크기로 맞춤
-          height: '14.75rem',
-          backgroundColor: 'rgba(0, 0, 0, 0.5)', // 배경 색상
-          padding: '10px', // 내부 여백
-          zIndex: 1,
-          overflow: 'hidden' // 다른 요소 위에 표시
-        }}
-      >
-        <h2 className="title">{movie.title}</h2>
-        <p className="overview">{movie.overview}</p>
+        <div className="overview-container">
+          <h2 className="title">{movie.title}</h2>
+          <p className="overview">{movie.overview}</p>
+        </div>
       </div>
       <div className="inform-container">
         <h2 className="title">{movie.title}</h2>
@@ -41,3 +26,4 @@ function Movie({ movie }) {
 }
 
 export default Movie;
+
